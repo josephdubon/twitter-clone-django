2fa5598be3cf5a3e2537ba397743cf3844d7fa4c
Joseph Dubon <josephdubon@pm.me>
2021-03-01 10:27:13 -0800

    Added custom redirect for after login and logout.

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -107,16 +107,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login and logout
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
