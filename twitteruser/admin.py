6c5e6f9c7611414b7f0377994bbddd138101d73a
Joseph Dubon <josephdubon@pm.me>
2021-03-01 11:58:11 -0800

    Added, registered custom TwitterUser admin class

--- twitteruser/admin.py
+++ twitteruser/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import TwitterUserChangeForm, TwitterUserCreationForm
+from .models import TwitterUser
+
+
+class TwitterUserAdmin(UserAdmin):
+    add_form = TwitterUserCreationForm
+    form = TwitterUserChangeForm
+    model = TwitterUser
+    list_display = ['email', 'username', ]
+
+
+admin.site.register(TwitterUser, TwitterUserAdmin)
