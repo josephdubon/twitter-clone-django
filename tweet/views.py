94f15a342432f8f378cf0493f863c893ce31a8f9
Joseph Dubon <josephdubon@pm.me>
2021-03-01 20:28:44 -0800

    Created and locked in views for tweet_home_view, tweet_list_view (this will be a custom order or view pate), tweet_detail_view, create_tweet, author_detail_view. All configured, tested, and in working order.

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
+from .models import Tweet
+from .forms import CreateTweetForm
+from twitterclone import settings
 
-# Create your views here.
+
+# Home tweet list view
+@login_required
+def tweet_home_view(request):
+    tweets = Tweet.objects.all().order_by('-create_time')
+    # total_tweets = Tweet.objects.filter(author=settings.AUTH_USER_MODEL).count()
+    return render(request, 'home.html', {
+        'tweets': tweets,
+        # 'total_tweets': total_tweets,
+    }
+                  )
+
+
+# Tweet list view
+@login_required
+def tweet_list_view(request):
+    tweets = Tweet.objects.all().order_by('-create_time')
+    return render(request, 'tweet_list.html', {
+        'tweets': tweets,
+    }
+                  )
+
+
+# Tweet detail view
+def tweet_detail_view(request, tweet_id):
+    tweet = Tweet.objects.get(id=tweet_id)
+
+    return render(request, 'tweet_detail.html', {
+        'tweet': tweet
+    }
+                  )
+
+
+# Create tweet view
+@login_required
+def create_tweet(request):
+    context = {}
+
+    if request.method == "POST":
+        form = CreateTweetForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            new_ticket = Tweet.objects.create(
+                title=data['title'],
+                author=request.user,
+                body=data['body'],
+            )
+            if new_ticket:
+                return HttpResponseRedirect(reverse('home'))
+
+    form = CreateTweetForm()
+    context.update({'form': form})
+    return render(
+        request,
+        'create_tweet.html',
+        context
+    )
+
+
+# Author detail view
+def author_detail_view(request, author_id):
+    author_obj = Tweet.objects.filter(author=author_id).first().author
+    tweets = Tweet.objects.filter(author_id=author_id).order_by('-create_time')
+
+    return render(request, 'author_detail.html', {
+        'author': author_obj,
+        'tweets': tweets
+    }
+                  )
