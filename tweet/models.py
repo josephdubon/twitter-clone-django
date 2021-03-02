3f83d8b9a01ee714d75a94cf9587b81249468c6e
Joseph Dubon <josephdubon@pm.me>
2021-03-01 22:04:39 -0800

    Created and commented out Author class
    Took 12 seconds

--- tweet/models.py
+++ tweet/models.py
@@ -5,6 +5,17 @@ from django.utils import timezone
 from twitterclone import settings
 
 
+# class Author(models.Model):
+#     user = models.OneToOneField(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,
+#         null=True,
+#         blank=True)
+#
+#     def __str__(self):
+#         return self.user
+#
+
 class Tweet(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -17,4 +28,4 @@ class Tweet(models.Model):
     create_time = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
-        return f'({self.author} | {self.title}| {self.body}| {self.create_time})'
+        return f'({self.author} |{self.title} | {self.body})'
