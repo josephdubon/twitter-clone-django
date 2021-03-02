95a42c3e5e5b3ad4143a1ef62e84225515b485e9
Joseph Dubon <josephdubon@pm.me>
2021-03-01 20:26:41 -0800

    Prettier formatting to file

--- twitteruser/models.py
+++ twitteruser/models.py
@@ -1,11 +1,10 @@
+from twitterclone import settings
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class TwitterUser(AbstractUser):
     pass
 
-    # add additional fields in here
 
-    def __str__(self):
-        return self.username
+def __str__(self):
+    return self.username
