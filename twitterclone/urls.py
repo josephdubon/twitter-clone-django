25f1965403c399d2ec565de89b55f75df30d817d
Joseph Dubon <josephdubon@pm.me>
2021-03-01 20:27:24 -0800

    Added path for tweets and re-created path for home with home view.

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
+from tweet import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('authentication.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('tweet/', include('tweet.urls')),
+    path('', views.tweet_home_view, name='home'),
 ]
