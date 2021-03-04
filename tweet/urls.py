c2246c032983d4ad308a23ac7a12ec3f9bb05006
Joseph Dubon <josephdubon@pm.me>
2021-03-04 13:26:39 -0800

    Removed author page url, moved to twitterusers urls
    Took 51 seconds

--- tweet/urls.py
+++ tweet/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from tweet import views
 
 urlpatterns = [
-    path('author/<int:author_id>/', views.author_detail_view, name='author_detail'),
     path('<int:tweet_id>/', views.tweet_detail_view, name='tweet_detail'),
     path('create-tweet/', views.create_tweet, name='create_tweet'),
     path('', views.tweet_list_view, name='tweet_list'),
