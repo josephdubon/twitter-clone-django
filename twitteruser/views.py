366e9975f0a401fd6c8dd37ad2cfb5ae663b8344
Joseph Dubon <josephdubon@pm.me>
2021-03-04 13:27:41 -0800

    Created author_detail_view
    Took 13 seconds

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 
-# Create your views here.
+from tweet.models import Tweet
+from .models import TwitterUser
+
+
+# Author detail view
+def author_detail_view(request, author_id):
+    author_obj = TwitterUser.objects.get(id=author_id)
+    tweets = Tweet.objects.filter(author=author_id).order_by('-create_time')
+
+    return render(request, 'author_detail.html', {
+        'author': author_obj,
+        'tweets': tweets
+    }
+                  )
