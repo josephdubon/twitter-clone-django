164db6a08093b4679f34c56bb34ef2a57fb28789
Joseph Dubon <josephdubon@pm.me>
2021-03-04 20:23:12 -0800

    Commented out custom user admin
    Took 3 hours 54 minutes

--- twitteruser/admin.py
+++ twitteruser/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from .forms import TwitterUserChangeForm, TwitterUserCreationForm
+# from django.contrib.auth.admin import UserAdmin
+#
+# from .forms import TwitterUserChangeForm, TwitterUserCreationForm
 from .models import TwitterUser
 
+# class TwitterUserAdmin(UserAdmin):
+#     add_form = TwitterUserCreationForm
+#     form = TwitterUserChangeForm
+#     model = TwitterUser
+#     list_display = [
+#         'username',
+#         'email',
+#     ]
+#
 
-class TwitterUserAdmin(UserAdmin):
-    add_form = TwitterUserCreationForm
-    form = TwitterUserChangeForm
-    model = TwitterUser
-    list_display = [
-        'username',
-        'email',
-    ]
-
-
-admin.site.register(TwitterUser, TwitterUserAdmin)
+# admin.site.register(TwitterUser, TwitterUserAdmin)
+admin.site.register(TwitterUser)
