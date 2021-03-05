3d22563f0da471384aa6def6291a50193fce3df5
Joseph Dubon <josephdubon@pm.me>
2021-03-04 20:25:26 -0800

    Created user urls.py
    Took 16 seconds

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from twitteruser import views
 
+app_name = 'twitteruser'
+
 urlpatterns = [
-    path('<int:author_id>/', views.author_detail_view, name='author_detail'),
+    path('listall/', views.TwitterUserListView.as_view(), name='twitter_list_view'),
+    path('<pk>/', views.TwitterUserDetailView.as_view(), name='user_detail_view'),
+    # path('follow/<str:user_name>', views.follow_view, name='follow_view'),
 ]
