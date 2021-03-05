6ea9f3cb8691da7786c187862f7bde641f0b5c2b
Joseph Dubon <josephdubon@pm.me>
2021-03-04 20:24:37 -0800

    Added update and create time
    Took 19 seconds

--- tweet/models.py
+++ tweet/models.py
@@ -1,20 +1,7 @@
 from django.db import models
-from django.urls import reverse
-from django.utils import timezone
 
 from twitterclone import settings
 
-
-# class Author(models.Model):
-#     user = models.OneToOneField(
-#         settings.AUTH_USER_MODEL,
-#         on_delete=models.CASCADE,
-#         null=True,
-#         blank=True)
-#
-#     def __str__(self):
-#         return self.user
-#
 
 class Tweet(models.Model):
     author = models.ForeignKey(
@@ -25,7 +12,11 @@ class Tweet(models.Model):
     )
     title = models.CharField(max_length=40)
     body = models.TextField(max_length=140)
-    create_time = models.DateTimeField(default=timezone.now, blank=True)
+    update_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'({self.author} |{self.title} | {self.body})'
+
+    class Meta:
+        ordering = ('-create_time',)
