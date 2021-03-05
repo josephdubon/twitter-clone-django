c81eb761c687a5e450e943c5c1ef052c3bf84bb9
Joseph Dubon <josephdubon@pm.me>
2021-03-04 20:24:59 -0800

    Added followers many to many field.
    Took 21 seconds

--- twitteruser/models.py
+++ twitteruser/models.py
@@ -1,10 +1,23 @@
-from twitterclone import settings
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class TwitterUser(AbstractUser):
-    pass
+    followers = models.ManyToManyField(
+        'self',
+        related_name='follows',
+        symmetrical=False,
+        blank=True
+    )
+    bio = models.TextField(default='User has not written bio yet...')
+    update_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
 
+    def twitteruser_tweets(self):
+        return self.twitteruser_tweets()
 
-def __str__(self):
-    return self.username
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ('-create_time',)
