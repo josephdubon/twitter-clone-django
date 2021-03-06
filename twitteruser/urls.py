d8a5ca3658183951c84bd04c1035d676892f007b
Joseph Dubon <josephdubon@pm.me>
2021-03-05 18:24:00 -0800

    Added paths for follow, unfollow, and new user detail view.
    Took 30 seconds

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -5,6 +5,7 @@ app_name = 'twitteruser'
 
 urlpatterns = [
     path('listall/', views.TwitterUserListView.as_view(), name='twitter_list_view'),
-    path('<pk>/', views.TwitterUserDetailView.as_view(), name='user_detail_view'),
-    # path('follow/<str:user_name>', views.follow_view, name='follow_view'),
+    path('<int:user_id>/', views.twitter_user_detail, name='user_detail_view'),
+    path('follow/<int:user_id>/', views.follow_twitteruser, name='follow_view'),
+    path('unfollow/<int:user_id>/', views.unfollow_twitteruser, name='unfollow_view'),
 ]
